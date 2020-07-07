1939eaa5b4675eb3e1db660362094bd2b8cc5031
Maxim <lostrandg@gmail.com>
2020-07-07 20:58:21 +0500

    add tests config

---
+++ test/model.js
@@ -0,0 +1,8 @@
+process.env.NODE_ENV = 'test';
+
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
