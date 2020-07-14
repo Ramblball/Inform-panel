00aba6596b7aa9bf26d4ca7694d2d31baad657b6
Maxim <lostrandg@gmail.com>
2020-07-14 14:30:30 +0500

    add album sender

--- routes/album.js
+++ routes/album.js
@@ -2,14 +2,16 @@ const router = require('express').Router();
 
 const Album = require('../models/album');
 
-router.get('/', (req, res, next) => {
+const sendAlbums = (req, res, next) => {
     Album.find({ user: req.user }, 'name comment hide end', (err, albums) => {
         if (err === null)
             res.send(albums);
         else
             next(err);
     });
-});
+}
+
+router.get('/', sendAlbums);
 
 router.post('/create', (req, res, next) => {
     const data = req.body;
@@ -18,10 +20,9 @@ router.post('/create', (req, res, next) => {
     album.save(err => {
         if (err === null)
             next(err.errors);
-        else
-            res.sendStatus(200);
+        next();
     });
-});
+}, sendAlbums);
 
 router.put('/update', (req, res, next) => {
     Album.findById({ _id: req.params.id }, (err, album) => {
@@ -30,17 +31,17 @@ router.put('/update', (req, res, next) => {
         Object.assign(album.toObject(), req.body).save(err => {
             if (err !== null)
                 next(err.errors);
-            res.sendStatus(200);
+            next();
         });
     });
-});
+}, sendAlbums);
 
 router.delete('/remove', (req, res, next) => {
     Album.deleteOne({ '_id': req.params.id }, err => {
         if (err !== null)
             next(err);
-        res.sendStatus(200);
+        next();
     });
-});
+}, sendAlbums);
 
 module.exports = router;
--- routes/router.js
+++ routes/router.js
@@ -7,12 +7,12 @@ const user = require('./users');
 
 router.use('/', auth);
 
-// router.use('/', (req, res, next) => {
-//   if (req.isAuthenticated())
-//     next();
-//   else
-//     res.redirect('/login');
-// });
+router.use('/', (req, res, next) => {
+  if (req.isAuthenticated())
+    next();
+  else
+    res.redirect('/login');
+});
 
 router.use('/', user);
 router.use('/album', album);
