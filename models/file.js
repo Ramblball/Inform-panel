1bcff5d9f8ded9c8d983c0da30ab92c6fed0395e
Maxim <lostrandg@gmail.com>
2020-07-20 15:31:14 +0500

    update file schema

--- models/file.js
+++ models/file.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema({
-    systemName: { type: String, maxlength: 64, required: true },
+    name: { type: String, maxlength: 64, required: true },
     type: { type: Boolean, required: true },
 
     hide: { type: Boolean, required: true, default: false },
