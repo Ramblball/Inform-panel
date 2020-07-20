255ac2137e9e7b53db18a3c0cabe4d22174f7dcd
keshakostya <kozloffkesha@gmail.com>
2020-07-20 23:47:14 +0500

    Still moving to uikit

--- view/JS/back.js
+++ view/JS/back.js
@@ -1,239 +1,121 @@
-'use strict'
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
-    let view = new View();
-    view.setUp();
-});
+    let control = new Control();
+    control.setUp();
+})
 
-class View {
 
-    constructor() {
-        this._model = new Model();
-        this._currentSection = 0;
-        this._sectionsList = {
-            0: () => {
-                this._setAlbums();
-                this._currentSection = 0;
-            },
-            1: () => {
-                this._currentSection = 1;
-            }
+class Control {
+    constructor() {}
+
+    setUp() {
+        this.setUpButtons();
+        this.setAlbums();
+    }
+
+    setUpButtons() {
+        document.getElementById('logOutBtn').onclick = () => {
+            fetch('/logout')
+                .then(res => console.log(res))
+                .catch(er => console.log(er));
+        }
+        document.getElementById('createAlbumBtn').onclick = () => {
+
         }
     }
 
-    setUp() {
-        console.log('Loading the page and setting up elements...');
-        this._setUpButtons();
-        this._setAlbums();
-        this._setSectionListButtons();
-        this._setWindowListener();
-        console.log('The page and all its elements ' +
-            "were successfully loaded!");
+    disableButton(buttonId) {
+        document.getElementById(buttonId).setAttribute('disabled', '');
     }
 
-    _setUpButtons() {
-        document.getElementById('logOutBtn').onclick = () => {}
-        document.getElementById('helpBtn').onclick = () =>
-            console.debug('Help');
+
+
+    enableButton(buttonId) {
+        document.getElementById(buttonId).removeAttribute('disabled');
     }
 
-    _setWindowListener() {
-        window.addEventListener("click", event => {
-            if (!event.target.matches(".dropdown") && !event.target.matches(".drop-btn")) {
-                this._closeAllDropdowns()
-            }
-            // if (event.target.matches(".modal-overlay")) {
-            //     closeAllModals();
-            // }
-        })
+    setInputCheckers() {
+        document.getElementById('newAlbumName').onchange = () => {
+            let albumName = document.getElementById('newAlbumName').value;
+            if (albumName.length == 0)
+                this.disableButton('createAlbumBtn');
+            else
+                this.enableButton('createAlbumBtn');
+        }
+
+        document.getElementById('newAlbumDate').onchange = () => {
+            let albumName = document.getElementById('newAlbumDate').value;
+            if (albumName.length == 0)
+                this.disableButton('createAlbumBtn');
+            else
+                this.enableButton('createAlbumBtn');
+        }
     }
 
-    _setAlbums() {
-        let albumsPromise = this._model.Albums;
+    setAlbums() {
+        let albumsPromise = this.getAlbumsPromise();
         albumsPromise.then(albums => {
             if (albums.length == 0)
                 return
-            let albumGrid = document.createElement('div');
-            albumGrid.setAttribute('class', 'album-grid');
-            albums.forEach(albumInfo => {
-                let albumDiv = this._createAlbumDom(albumInfo);
-                albumGrid.appendChild(albumDiv);
+            let albumContainer = document.createElement('div');
+            albumContainer.setAttribute('uk-grid', '');
+            albumContainer.setAttribute('class', 'uk-grid-column-small uk-child-width-1-6 uk-text-center');
+            document.getElementById('main').appendChild(albumContainer);
+            albums.forEach(el => {
+                let albumDom = this.createAlbumDom(el);
+                albumContainer.appendChild(albumDom);
+                this.createAlbumDropdown(el);
             });
-            let albumColumns = document.createElement('div');
-            albumColumns.appendChild(albumGrid);
-            document.querySelector('.container').appendChild(albumGrid);
-            let fileInput = document.createElement('input');
-            fileInput.setAttribute('id', 'fileInput');
-            fileInput.style.display = 'none';
-            fileInput.onclick = () => { console.log('Открывается модальное окно для добавления файла в альбом') } // TODO: Надо как-то брать id альбома при нажатии, замыкание
-            document.querySelector('.container').appendChild(fileInput);
         });
-
     }
 
-    _setSectionListButtons() {
-        for (let i = 0; i < 2; i++) {
-            let num = i;
-            document.getElementById(`section-${i}`).onclick = () => {
-                this._switchSection(num);
-            }
-        }
-    }
-
-    _createAlbumDom(albumInfo) {
-        let albumDiv = document.createElement('div');
-        albumDiv.setAttribute('id', albumInfo._id);
-        albumDiv.setAttribute('class', 'uk-card uk-card-default');
-        let albumText = document.createElement('p');
-        albumText.textContent = albumInfo.name;
-        albumDiv.appendChild(albumText);
-        let albumDrop = this._createAlbumDropdown(albumInfo);
-        albumDiv.appendChild(albumDrop);
-        albumDiv.oncontextmenu = (e) => {
+    createAlbumDom(album) {
+        let albumDom = document.createElement('div');
+        albumDom.setAttribute('id', `album_${album._id}`);
+        let albumCard = document.createElement('div');
+        albumCard.setAttribute('class', 'uk-card uk-card-default uk-card-body');
+        albumCard.textContent = album.name;
+        albumDom.appendChild(albumCard);
+        albumDom.oncontextmenu = (e) => {
             e.preventDefault();
-            this._openDropdown(e, albumDrop.id);
-        };
-        return albumDiv;
-    }
-
-    _closeAllSections() {
-        let container = document.querySelector('.container');
-        if (container.children.length < 2)
-            throw new errors.UnexpectedError('Less then two children in ' +
-                "container, omg!");
-        container.removeChild(container.lastElementChild);
-    }
-
-    _switchSection(section) {
-        if (!(section in this._sectionsList))
-            throw new errors.UnexpectedError(`Key ${section} not found`);
-        if (section === this._currentSection)
-            return;
-        this._closeAllSections();
-        this._sectionsList[section]();
-    }
-
-    _createBasicDropdown(id, disabledAnchorCounts, anchorCounts) {
-        let dropdownDom = document.createElement('div');
-        dropdownDom.setAttribute('class', 'dropdown');
-        dropdownDom.setAttribute('id', id);
-        for (let i = 0; i < anchorCounts; i++) {
-            let anchorDom = document.createElement('a');
-            if (i < disabledAnchorCounts)
-                anchorDom.setAttribute('class', 'disabled')
-            if (i == disabledAnchorCounts)
-                dropdownDom.appendChild(document.createElement('hr'));
-            dropdownDom.appendChild(anchorDom);
+            UIkit.dropdown(document.getElementById(`drop_${album._id}`)).show();
         }
-        return dropdownDom;
+        return albumDom;
     }
 
-    _setUpCreationDropdown() {
-        let dropdownDom = this._createBasicDropdown('createDropdown', -1, 2);
-        dropdownDom.childNodes[0].textContent = 'Альбом';
-        dropdownDom.childNodes[0].onclick = () => { this._openModal('createAlbumModal') };
-        dropdownDom.childNodes[1].textContent = 'Объявление';
-        dropdownDom.childNodes[1].onclick = () => { console.log('Тут типо открыается модалка для создания объявления') };
-        return dropdownDom();
+    createAlbumDropdown(album) {
+        let dropDowndom = document.createElement('div');
+        dropDowndom.setAttribute('id', `drop_${album._id}`);
+        let anchorTexts = [
+            album.name,
+            album.comment,
+            'Добавить файл',
+            'Изменить комментарий',
+            'Изменить дату удаления',
+            album.hide ? 'Показать' : 'Скрыть',
+            'Удалить'
+        ]
+        for (let i = 0; i < 7; i++) {
+            let albumDropLi = document.createElement('li');
+            let albumDropAnchor = document.createElement('a');
+            albumDropAnchor.textContent = anchorTexts[i];
+            albumDropLi.appendChild(albumDropAnchor);
+            dropDowndom.appendChild(albumDropLi);
+        }
+        document.getElementById(`album_${album._id}`).appendChild(dropDowndom);
+        console.log(document.getElementById(`drop_${album._id}`))
+        UIkit.dropdown(dropDowndom, {
+
+        });
     }
 
-    _createAlbumDropdown(album) {
-        let dropdownDom = this._createBasicDropdown(`${album._id}_drop`, 2, 7);
-        dropdownDom.childNodes[0].textContent = `Название: ${album.name}`;
-        dropdownDom.childNodes[1].textContent = `Комментарий: ${album.comment}`;
-        dropdownDom.childNodes[3].textContent = 'Изменить комментарий';
-        dropdownDom.childNodes[3].onclick = () => { console.log('changing comment') };
-        dropdownDom.childNodes[4].textContent = album.hide ? 'Показать' : 'Скрыть';
-        dropdownDom.childNodes[4].onclick = () => { console.log('Изменяю видимость') };
-        dropdownDom.childNodes[5].textContent = 'Изменить дату удаления';
-        dropdownDom.childNodes[5].onclick = () => { console.log('Изменяю дату удаления') };
-        dropdownDom.childNodes[6].textContent = 'Добавить файл';
-        dropdownDom.childNodes[6].onclick = () => { console.log('Добавляю файл') };
-        dropdownDom.childNodes[7].textContent = 'Удалить';
-        dropdownDom.childNodes[7].onclick = () => { console.log('Удаляю') };
-        return dropdownDom
-    }
-
-    _createBasicModalWindow() {
-        let modalDom = document.getElementById('div');
-        modalDom.setAttribute('class', 'modal');
-        let modalContent = document.createElement('div');
-        modalContent.setAttribute('class', 'modal-content');
-        modalDom.appendChild(modalContent);
-        return modalDom;
-    }
-
-    _createAlbumCreateModal() {
-        let modal = this._createBasicModalWindow();
-        modal.setAttribute('id', 'createAlbumModal');
-        let nameInput = document.createElement('input');
-        nameInput.setAttribute('type', 'date');
-        let dateInput = document.createElement('input');
-        dateInput.setAttribute('type', 'date');
-        modal.childNodes[0].appendChild(nameInput);
-        modal.childNodes[0].appendChild(dateInput);
-        document.body.appendChild(modal);
-    }
-
-    _openDropdown(event, dropdownId) {
-        this._closeAllDropdowns();
-        document.getElementById(dropdownId).style.display = 'block';
-        document.getElementById(dropdownId).style.left = event.pageX + "px";
-        document.getElementById(dropdownId).style.top = event.pageY + "px";
-    }
-
-    _closeAllDropdowns() {
-        document.querySelectorAll(".dropdown")
-            .forEach(dropdown => {
-                dropdown.style.display = "none";
-            });
-    }
-
-    _openModal(modalId) {
-        document.getElementById(modalId).style.display = 'block';
-    }
-
-    _closeAllModal(modalId) {
-        document.getElementById(modalId).style.display = 'none';
-    }
-}
-
-class Model {
-
-    constructor() {
-        this._albums = [];
-        this._curAlbum = {};
-        this._albumContent = [];
-        this._curFile = {};
-        this._texts = [];
-    }
-
-    get Albums() {
-        return this.getAlbums();
-    }
-
-    async getAlbums() {
+    async getAlbumsPromise() {
         try {
-            const res = await fetch('/album/');
-            const data = await res.json();
-            this._albums = data;
-            return data;
+            const res = await fetch('/album');
+            if (!(res.ok))
+                throw new Error(res);
+            return await res.json();
         } catch (er) {
-            console.error(er);
+            return console.error(er);
         }
-    }
-
-    createNewAlbum(album) {
-        fetch('/album/create', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json;charset=utf-8' },
-                body: JSON.stringify(album)
-            })
-            .then(res => console.log(res))
-            .catch(er => {
-                throw er;
-            })
     }
 }
--- view/JS/main.js
+++ view/JS/main.js
@@ -1,125 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let control = new Control();
-    control.setUp();
-})
+    let a = new A();
+    a.loadPageElements();
+});
 
 
-class Control {
-    constructor() {}
-
-    setUp() {
-        this.setUpButtons();
-        this.setAlbums();
+class A {
+    loadPageElements() {
+        this.showAlbums();
     }
 
-    setUpButtons() {
-        document.getElementById('logOutBtn').onclick = () => {
-            fetch('/logout')
-                .then(res => console.log(res))
-                .catch(er => console.log(er));
-        }
-        document.getElementById('createAlbumBtn').onclick = () => {
-
-        }
-    }
-
-    disableButton(buttonId) {
-        document.getElementById(buttonId).setAttribute('disabled', '');
-    }
-
-
-
-    enableButton(buttonId) {
-        document.getElementById(buttonId).removeAttribute('disabled');
-    }
-
-    setInputCheckers() {
-        document.getElementById('newAlbumName').onchange = () => {
-            let albumName = document.getElementById('newAlbumName').value;
-            if (albumName.length == 0)
-                this.disableButton('createAlbumBtn');
-            else
-                this.enableButton('createAlbumBtn');
-        }
-
-        document.getElementById('newAlbumDate').onchange = () => {
-            let albumName = document.getElementById('newAlbumDate').value;
-            if (albumName.length == 0)
-                this.disableButton('createAlbumBtn');
-            else
-                this.enableButton('createAlbumBtn');
-        }
-    }
-
-    setAlbums() {
-        let albumsPromise = this.getAlbumsPromise();
-        albumsPromise.then(albums => {
-            if (albums.length == 0)
-                return
-            let albumContainer = document.createElement('div');
-            document.body.appendChild(albumContainer);
-            albumContainer.setAttribute('uk-grid', '');
-            albumContainer.setAttribute('class', 'uk-grid-column-small uk-child-width-1-6@s uk-text-center');
-            albums.forEach(el => {
-                let albumDom = this.createAlbumDom(el);
-                albumContainer.appendChild(albumDom);
-                this.createAlbumDropdown(el);
+    showAlbums() {
+        this.getAlbumPromise()
+            .then(albums => {
+                if (albums.length == 0)
+                    return
+                let albumContainer = document.getElementById('mainField');
+                albumContainer.setAttribute('uk-grid', '');
+                albumContainer.classList.add('uk-child-width-1-5');
+                albums.forEach(el => {
+                    let albumDom = this.createAlbumDom(el);
+                    albumContainer.appendChild(albumDom);
+                    this.fillAlbumDropdown(el);
+                });
             });
-        });
     }
 
     createAlbumDom(album) {
         let albumDom = document.createElement('div');
-        albumDom.setAttribute('class', '');
         albumDom.setAttribute('id', `album_${album._id}`);
         let albumCard = document.createElement('div');
         albumCard.setAttribute('class', 'uk-card uk-card-default uk-card-body');
         albumCard.textContent = album.name;
         albumDom.appendChild(albumCard);
-        albumDom.oncontextmenu = (e) => {
-            e.preventDefault();
-            UIkit.dropdown(document.getElementById(`drop_${album._id}`)).show();
-        }
+        let albumDropdown = this.createAlbumDropdown(album);
+        albumDom.innerHTML += albumDropdown;
         return albumDom;
     }
 
-    createAlbumDropdown(album) {
-        let dropDowndom = document.createElement('div');
-        dropDowndom.setAttribute('id', `drop_${album._id}`);
-        let anchorTexts = [
-            album.name,
-            album.comment,
-            'Добавить файл',
-            'Изменить комментарий',
-            'Изменить дату удаления',
-            album.hide ? 'Показать' : 'Скрыть',
-            'Удалить'
-        ]
-        for (let i = 0; i < 7; i++) {
-            let albumDropLi = document.createElement('li');
-            let albumDropAnchor = document.createElement('a');
-            albumDropAnchor.textContent = anchorTexts[i];
-            albumDropLi.appendChild(albumDropAnchor);
-            dropDowndom.appendChild(albumDropLi);
+    fillAlbumDropdown(album) {
+        let drop = document.getElementById(`drop_${album._id}`);
+        console.log(drop)
+        drop.childNodes[7].onclick = () => {
+            let url = new URL(window.location.href + 'remove'),
+                params = { id: album._id }
+            Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+            fetch(url, {
+                    method: 'DELETE'
+                })
+                .then(res => {
+                    console.log(res);
+                    this.showAlbums();
+                })
+                .catch(er => console.error(er));
         }
-        document.getElementById(`album_${album._id}`).appendChild(dropDowndom);
-        console.log(document.getElementById(`drop_${album._id}`))
-        UIkit.dropdown(dropDowndom, {
-            'pos': 'bottom-justify',
-            'boundary': '.boundary-align',
-            'boundary-align': true,
-            'delay-hide': 100
-        });
     }
 
-    async getAlbumsPromise() {
+    createAlbumDropdown(album) {
+        const dropdownMarkup = `
+            <div uk-dropdown id='drop_${album._id}'>
+                <ul class='uk-nav uk-dropdown-nav'>
+                    <li>${album.name}</li>
+                    <li>${album.comment}</li>
+                    <li class="uk-nav-divider"></li>
+                    <li><a href='#'>Добавить файл</a></li>
+                    <li><a href='#'>Изменить комментарий</a></li>
+                    <li><a href='#'>Изменить дату удаления</a></li>
+                    <li><a href='#'>${album.hide ? 'Показать' : 'Скрыть'}</a></li>
+                    <li><a href='#'>Удалить</a></li>
+                </ul>
+            </div>
+        `
+        let parser = new DOMParser();
+        return parser.parseFromString(dropdownMarkup, 'text/html');
+    }
+
+    async getAlbumPromise() {
         try {
-            const res = await fetch('/album');
-            if (!(res.ok))
-                throw new Error(res);
+            let res = await fetch('/album');
             return await res.json();
-        } catch (er) {
-            return console.error(er);
+        } catch (e) {
+            throw e;
         }
     }
 }
