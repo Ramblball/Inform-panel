966a12633771b1e7a6a4744547044476c1ec6886
Maxim <lostrandg@gmail.com>
2020-07-21 05:45:40 +0500

    update file router
    add DELETE /file/remove
    and add error handling to other

--- routes/file.js
+++ routes/file.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const _ = require('lodash');
+const createError = require('http-errors');
 const uuid = require('uuid/v4');
 
 const Album = require('../models/album');
@@ -16,25 +17,37 @@ const getType = ext => {
     return undefined;
 }
 
-router.get('/', (req, res, next) => {
-    Album.findById(req.params._id, (err, album) => {
-        if (err === null)
-            res.status(200).send(album.files);
-        else
+const sendFiles = (req, res, next) => {
+    Album.findOne({ _id: req.params._id, user: req.user._id }, (err, album) => {
+        if (err !== null)
             next(err);
+        if (album === undefined)
+            next(createError(404));
+        res.status(200).send(album)
     });
-});
+}
+
+router.get('/', (req, res, next) => {
+    if (req.user._id === undefined)
+        next(createError(401));
+    else if (req.body.aid === undefined)
+        next(createError(400));
+    else 
+        next();
+}, sendFiles);
 
 router.post('/upload', (req, res, next) => {
-    if (!req.files) {
+    if (req.files === undefined) {
         next({
             status: 400,
             message: 'No file uploaded'
         });
     } else {
-        Album.findById(req.params._id, (err, album) => {
+        Album.findOne({ _id: req.params.id, user: req.user._id }, (err, album) => {
             if (err !== null)
                 next(err);
+            else if (album === undefined)
+                next(createError(404));
             else {
                 _.forEach(_.keysIn(req.files.files), key => {
                     let file = req.files.files[key];
@@ -66,20 +79,40 @@ router.post('/upload', (req, res, next) => {
 });
 
 router.put('/update', (req, res, next) => {
-    Album.findById(req.body.aid, (err, album) => {
+    Album.findOne({ _id: req.body.aid, user: req.user._id }, (err, album) => {
         if (err !== null)
             next(err);
+        else if (album === undefined)
+            next(createError(404));
         else {
             let file = album.file.id(req.body.fid);
             Object.assign(file, req.body).save(err => {
                 if (err !== null)
                     next(err.errors);
-                else 
-                    res.status(200).send(album.file)
+                else
+                    next();
             });
 
         }
     });
-});
+}, sendFiles);
+
+router.delete('/remove', (req, res, next) => {
+    Album.findOne({ _id: req.body.aid, user: req.user._id }, (err, album) => {
+        if (err !== null)
+            next(err);
+        else if (album === undefined)
+            next(createError(404));
+        else {
+            album.file.id(req.body.fid).remove();
+            album.save(err => {
+                if (err !== null)
+                    next(err.errors);
+                else 
+                    next();
+            })
+        }
+    });
+}, sendFiles)
 
 module.exports = router;
