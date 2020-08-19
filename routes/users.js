f30d481a083e8d5408f822b31b017da005ffe164
Maxim <lostrandg@gmail.com>
2020-08-19 18:09:11 +0500

    add user CRUD operations with apidoc

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,17 @@
 const router = require('express').Router();
 const createError = require('http-errors');
 const User = require('../models/user');
-const path = require('path');
 
-/**
- * @api {get} / Request main page
- * @apiName GetMainPage
- * @apiGroup User
- * 
- * @apiPermission Autorized
- * 
- * @apiSuccess (200) {String} text Main page
- * 
- * @apiError (404) {Number} status File not found
-*/
-router.get('/', (req, res, next) => {
-  try {
-    res.status(200).sendFile(path.join(__dirname, '..', 'view', 'html', 'main.html'));
-  } catch (error) {
-    next(createError(404));
-  }
-});
+const sendUsers = (req, res, next) => {
+  if (!req.user.status)
+    next(createError(403, 'Common user'));
+  User.find({}, (err, users) => {
+    if (err)
+      next(createError(500));
+    else
+      res.status(200).send(users);
+  });
+}
 
 /**
  * @api {get} /info Request info about user
@@ -39,8 +30,6 @@ router.get('/', (req, res, next) => {
  *         ["patronomic": "Patronomic"]
  *       }
  *     }
- * 
- * @apiError (404) {Number} status File not found
 */
 router.get('/info', (req, res, next) => {
   res.json({
@@ -50,36 +39,88 @@ router.get('/info', (req, res, next) => {
 });
 
 /**
- * @api {put} /update Change user password
- * @apiName UpdatePassword
+ * @api {get} /user Request array of users
+ * @apiName GetUsers
  * @apiGroup User
  * 
- * @apiParam {String} old Old password
- * @apiParam {String} new New password
+ * @apiPermission Autorized
+ * @apiPermission Admin
+ * 
+ * @apiSuccess (200) {Object[]} body Users array
+ * 
+ * @apiError (403) {Number} status Common user
+ * @apiError (500) {Number} status Server error
+*/
+router.get('/', sendUsers);
+
+/**
+ * @api {put} /update/:id Upadate user data
+ * @apiName UserUpdate
+ * @apiGroup User
+ * 
+ * @apiParam {ObjectId} id User Id
+ * @apiParam {Boolean} [status]
+ * @apiParam {String} [login]
+ * @apiParam {String{..32}} [name[first]]
+ * @apiParam {String{..32}} [name[last]]
+ * @apiParam {String{..32}} [name[patronomic]]
+ * @apiParam {String} [old] Old password
+ * @apiParam {String} [new] New password
  * 
  * @apiPermission Autorized
+ * @apiPermission Admin
  * 
- * @apiSuccess (200) {Number} status Password changed
+ * @apiSuccess (200) {Number} status data updated
  * 
  * @apiError (400) {Number} status Invalid request
+ * @apiError (403) {Number} status Common user
  * @apiError (404) {Number} status User not found
  * @apiError (500) {Number} status Server error
 */
 router.put('/update', (req, res, next) => {
-  User.findById(req.user._id, (err, user) => {
+  if (!req.user.status)
+    next(createError(403));
+  User.findById(req.query.id, (err, user) => {
     if (err) next(createError(500, err));
     if (!user) next(createError(404));
-    if (user.validPassword(req.body.old)) {
-      user.setPssword(req.body.new);
-      user.save(err => {
-        if (err)
-          next(createError(400, err));
-        res.sendStatus(200);
-      });
+    let updated = Object.assign(user, req.body);
+    if (req.body.old && req.body.new) {
+      if (user.validPassword(req.body.old)) {
+        updated.setPassword(req.body.new);
+      }
+      else
+        next(createError(400, 'invalid password'));
     }
-    else
-      next(createError(400, 'invalid password'));
+    updated.save(err => {
+      if (err)
+        next(createError(400, err.errors));
+      next();
+    });
   });
-});
+}, sendUsers);
+
+/**
+ * @api {delete} /remove/:id Remove text
+ * @apiName RemoveText
+ * @apiGroup Text
+ * 
+ * @apiParam {ObjectId} id Text Id
+ * 
+ * @apiPermission Autorized
+ * 
+ * @apiSuccess (200) {Object[]} body Text array
+ * 
+ * @apiError (500) {Number} status Server error
+*/
+router.delete('/remove', (req, res, next) => {
+  if (!req.user.status)
+    next(createError(403));
+  User.deleteOne({ _id: req.query.id }, (err, info) => {
+    if (err)
+      createError(500, err);
+    else 
+      next();
+  });
+}, sendUsers);
 
 module.exports = router;
